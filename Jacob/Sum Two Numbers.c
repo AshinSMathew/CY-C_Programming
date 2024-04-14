9bced6c7710508734bebc9ae9398873a2cfc8956
Jacob Prasanth <166240794+jacobprasanth02@users.noreply.github.com>
2024-04-14 22:00:33 +0530

    Add files via upload

---
+++ Jacob/Sum Two Numbers.c
@@ -0,0 +1,12 @@
+// To sum two number
+#include <stdio.h>
+void main()
+{
+	float a,b;
+	printf("Enter first number");
+	scanf("%f",&a);
+	printf("Enter second number");
+	scanf("%f",&b);
+	printf("Number is %f",a+b);
+}
+
